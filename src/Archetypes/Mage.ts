0eac20e981b9958bf307aee979eb0caa6e05b3a8
Jhon Santana <jhonsstn@gmail.com>
2022-08-10 14:26:23 -0300

    refactor: put _nergyType initialization aou of the contructor of class Mage

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -2,11 +2,10 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 export default class Mage extends Archetype {
-  private _energyType: EnergyType;
+  private _energyType: EnergyType = 'mana';
   private static _instances = 0;
   constructor(name: string) {
     super(name);
-    this._energyType = 'mana';
     Mage._instances += 1;
   }
 
